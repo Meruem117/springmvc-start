d9e32bb21c8cde295f44fad3ee81b9b709bf83c8
Meruem117 <1572677319@qq.com>
2021-08-27 09:47:31 +0800

    add model, map, modelmap

--- src/main/java/start/mvc/controller/ScopeController.java
+++ src/main/java/start/mvc/controller/ScopeController.java
@@ -1,9 +1,13 @@
 package start.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @auther: Meruem117
@@ -13,7 +17,33 @@ import javax.servlet.http.HttpServletRequest;
 public class ScopeController {
     @RequestMapping("/servlet")
     public String servlet(HttpServletRequest request) {
-        request.setAttribute("msg", "hello");
+        request.setAttribute("msg", "hello from Servlet");
+        return "target";
+    }
+
+    @RequestMapping("/mav")
+    public ModelAndView mav() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("msg", "hello from ModelAndView");
+        mav.setViewName("target");
+        return mav;
+    }
+
+    @RequestMapping("/model")
+    public String model(Model model) {
+        model.addAttribute("msg", "hello from Model");
+        return "target";
+    }
+
+    @RequestMapping("/map")
+    public String map(Map<String, Object> map) {
+        map.put("msg", "hello from Map");
+        return "target";
+    }
+
+    @RequestMapping("/mm")
+    public String mm(ModelMap modelMap) {
+        modelMap.addAttribute("msg", "hello from ModelMap");
         return "target";
     }
 }
