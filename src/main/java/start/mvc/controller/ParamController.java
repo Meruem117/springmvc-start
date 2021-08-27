79dfb508f312984857df1fb405369a338fbab53d
Meruem117 <1572677319@qq.com>
2021-08-27 08:48:59 +0800

    update ParamController

--- src/main/java/start/mvc/controller/ParamController.java
+++ src/main/java/start/mvc/controller/ParamController.java
@@ -1,8 +1,11 @@
 package start.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import start.mvc.pojo.User;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
@@ -33,8 +36,26 @@ public class ParamController {
     }
 
     @RequestMapping("/requestParam")
-    public String requestParam(@RequestParam(value = "user", required = false, defaultValue = "name") String username) {
+    public String testRequestParam(@RequestParam(value = "user", required = false, defaultValue = "name") String username) {
         System.out.println(username);
+        return "target";
+    }
+
+    @RequestMapping("/requestHeader")
+    public String testRequestHeader(@RequestHeader("Host") String host) {
+        System.out.println(host);
+        return "target";
+    }
+
+    @RequestMapping("/cookieValue")
+    public String testCookieValue(@CookieValue("JSESSIONID") String sessionId) {
+        System.out.println(sessionId);
+        return "target";
+    }
+
+    @RequestMapping("/pojo")
+    public String testPOJO(User user) {
+        System.out.println(user);
         return "target";
     }
 }
